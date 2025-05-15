93b97114dfed96be18f349a5eb6e9458ae02631b
Farhan Tanvir <56257502+Immobile07@users.noreply.github.com>
2025-05-16 04:49:39 +0600

    Add files via upload

---
+++ l1/Task A.py
@@ -0,0 +1,7 @@
+n=int (input(""))
+for i in range(n):
+    nm=int(input(""))
+    if nm%2==0:
+        print(f'{nm} is an Even number.')
+    else:
+        print(f'{nm} is an Odd number.')
---
+++ l1/taskd.py
@@ -0,0 +1,4 @@
+n=int(input(""))
+for i in range(n):
+    nx=int(input(""))
+    print((nx*(nx+1))//2)
